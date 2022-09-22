61b9345e07317166945b42e70b370b15f346f2ee
Sangwon Ko <parania@npixel.co.kr>
2022-09-23 00:45:02 +0900

    ch3-03-newtype

---
+++ ch3-03-newtype/src/main.rs
@@ -0,0 +1,20 @@
+// # newtype 패턴에 대한 설명
+// - type alias 와 유사한 효과를 내면서, 완전히 별개의 타입으로 취급되게 하는 방법
+// - 단점은.. 모든 동작을 다 구현을 또 해줘야한다..
+
+struct Hostname(String);
+
+//type Hostname = String;   // 이렇게 하면 String을 쓰든 Hostname을 쓰든 프로그램이 정상 동자
+
+fn connect(host: Hostname) {
+    println!("connected to {}", host.0);
+}
+
+fn main() {
+    let ordinary_string = String::from("localhost");
+    //connect(ordinary_string); // 이것 불가
+
+    let host = Hostname(ordinary_string.clone());
+
+    connect(host);
+}
